8715d4ee2192c630fe710f0891cac311b2d35319
Gotha01 <72265241+Gotha01@users.noreply.github.com>
2022-06-16 18:07:33 +0200

    auth-user

--- food_substitution/models.py
+++ food_substitution/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    Category_name = models.CharField(unique=True)
+
+    def __str__(self):
+        return self.Category_name
+
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ManyToManyField(Categories)
