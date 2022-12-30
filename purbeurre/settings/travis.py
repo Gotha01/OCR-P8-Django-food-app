906d8e9586f385b23eb890f46f3935b1fd21b398
Gotha01 <72265241+Gotha01@users.noreply.github.com>
2022-12-31 00:22:32 +0100

    travis_conf

--- purbeurre/settings/travis.py
+++ purbeurre/settings/travis.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'purbeurre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
+        'NAME': 'travis_ci_test',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '',
